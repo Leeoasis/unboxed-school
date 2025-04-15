4e1b36b5f69cf54e74af0dd55411e911c8941530
Leslie <Kooliesk@gmail.com>
2025-04-15 18:08:40 +0200

    add coming soon page

--- src/App.js
+++ src/App.js
@@ -5,17 +5,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>
+          Unboxed Autism Christian School
+        </h1>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+
         >
-          Learn React
+          Under Construction, Coming Soon!
         </a>
       </header>
     </div>
