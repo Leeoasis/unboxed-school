ef4dbd173e98681b739ca316ba65813f4f26b160
Leslie <Kooliesk@gmail.com>
2025-04-17 16:24:58 +0200

    fix linter errors

--- src/App.js
+++ src/App.js
@@ -6,17 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        {/* <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p> */}
-        <a
-          className="App-link"
-          href="#"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <button className="App-link" onClick={() => {}}>
           Under Construction, Coming Soon!
-        </a>
+        </button>
       </header>
     </div>
   );
